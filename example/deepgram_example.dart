79ee7e72a72a1a63a740829aa8330ba1f934b118
Thibault <thibaultchatillon@gmail.com>
2024-03-15 15:01:15 +0100

    updated example

--- example/deepgram_example.dart
+++ example/deepgram_example.dart
@@ -1,35 +1,55 @@
-import 'dart:convert';
 import 'dart:io';
-
-import 'package:deepgram_speech_to_text/deepgram_speech_to_text.dart';
-import 'package:dotenv/dotenv.dart';
+import 'package:deepgram_speech_to_text/deepgram.dart';
 
 void main() async {
-  final env = DotEnv()..load();
+  // Get your API key from the Deepgram console if you don't have one https://console.deepgram.com/
+  String apiKey = "YOUR_API_KEY";
 
-  final apiKey = env.getOrElse("DEEPGRAM_API_KEY", () => throw Exception("No API Key found"));
-  final deepgram = Deepgram(apiKey);
-  final file = File('assets/jfk.wav');
+  // you can pass optional query parameters in :
+  // - the constructore for all requests
+  // - the method for a specific request
+  // reference : https://developers.deepgram.com/reference/listen-file
+  Map<String, dynamic> params = {
+    'model': 'nova-2-general',
+    'language': 'latest',
+    'filler_words': false,
+    'punctuation': true,
+  };
 
-  Stream<List<int>> audioStream = file.openRead();
+  Deepgram deepgram = Deepgram(apiKey, baseQueryParams: params);
+
+  // -------------------- From a file --------------------
+  File audioFile = File('audio.wav');
+
+  String json1 = await deepgram.transcribeFromFile(audioFile);
+  print(json1);
+
+  // -------------------- From a URL --------------------
+  String json2 = await deepgram.transcribeFromUrl('https://somewhere/audio.wav');
+  print(json2);
+
+  // -------------------- From raw data --------------------
+  String json3 = await deepgram.transcribeFromBytes(List.from([1, 2, 3, 4, 5]));
+  print(json3);
+
+  // -------------------- From a stream  --------------------
+  Stream<List<int>> audioStream = audioFile.openRead(); // mic.stream ...
+
+  Stream<String> jsonStream = deepgram.transcribeFromLiveAudioStream(audioStream);
+
+  jsonStream.listen((json) {
+    print(json);
+  });
+
+  // if you prefer to have more control over the stream:
 
   final DeepgramLiveTranscriber transcriber = deepgram.createLiveTranscriber(audioStream);
 
-  transcriber.jsonStream.listen((json) {
-    print('JSON: $json');
-    Map<String, dynamic> map = jsonDecode(json);
-    String transcript = map['channel']['alternatives'][0]['transcript'];
+  transcriber.start();
 
-    print('Transcript: $transcript');
-  }, onDone: () {
-    print('Done');
-  }, onError: (error) {
-    print('Error: $error');
+  transcriber.jsonStream.listen((json) {
+    print(json);
   });
 
-  await transcriber.start();
-
-  await Future.delayed(Duration(seconds: 5));
-
-  await transcriber.close();
+  transcriber.close();
 }
